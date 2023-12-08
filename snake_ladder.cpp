017564dcd743f4abcaf19430739fba29b4a5d77e
unknown <mobinaghashahi13@gmail.com>
2023-12-09 01:57:12 +0330

    create graphic for game and clean code

--- snake_ladder.cpp
+++ snake_ladder.cpp
@@ -4,8 +4,46 @@
 
 using namespace std;
 
+int snakeHead[8]={14,27,35,50,70,77,86,99};
+int snakeTail[8]={3,19,9,5,48,32,56,21};
+
+// Check for snakes and ladders
+int snakeHomeCheck(int position){
+    for (int index=0;index<=8;index++){
+        if(position==snakeHead[index])
+            return snakeTail[index];
+    }
+    return position;
+}
+
 int rollDice() {
     return rand() % 6 + 1;
+}
+
+void createMap(int position1,int position2){
+    cout<<endl;
+    cout<<endl;
+    for(int index=0;index<=100;index++){
+            for(int indexSnake=0;indexSnake<8;indexSnake++){
+                if(index==snakeHead[indexSnake])
+                {
+                    cout<<"S";
+                    goto endS;
+                }
+            }
+        if(position1==index)
+            cout<<"A";
+        else if(position2==index)
+            cout<<"B";
+        else if(index==snakeTail[index])
+            cout<<"T";
+        else
+            cout<<"_";
+            endS:;
+    }
+    cout<<"END";
+    cout<<endl;
+    cout<<endl;
 }
 
 int main() {
@@ -14,6 +52,7 @@ int main() {
     int currentPlayer = 1; // Player 1 starts
 
     while (player1 < 100 && player2 < 100) {
+             createMap(player1,player2);
         // Switch players
         if (currentPlayer == 1) {
             cout << "Player 1's turn. Press Enter to roll the dice...";
@@ -23,6 +62,7 @@ int main() {
             player1 += dice;
             cout << "Player 1 is now at position " << player1 << endl;
             currentPlayer = 2;
+            player1=snakeHomeCheck(player1);
         } else {
             cout << "Player 2's turn. Press Enter to roll the dice...";
             cin.get();
@@ -31,66 +71,14 @@ int main() {
             player2 += dice;
             cout << "Player 2 is now at position " << player2 << endl;
             currentPlayer = 1;
-        }
-
-        // Check for snakes and ladders
-        if (player1 == 14) {
-            player1 = 3;
-            cout << "Player 1 got a ladder! Climbing to position 3." << endl;
-        } else if (player1 == 27) {
-            player1 = 19;
-            cout << "Player 1 got a ladder! Climbing to position 19." << endl;
-        } else if (player1 == 35) {
-            player1 = 9;
-            cout << "Player 1 got a snake! Sliding down to position 9." << endl;
-        } else if (player1 == 50) {
-            player1 = 5;
-            cout << "Player 1 got a snake! Sliding down to position 5." << endl;
-        } else if (player1 == 70) {
-            player1 = 48;
-            cout << "Player 1 got a snake! Sliding down to position 48." << endl;
-        } else if (player1 == 77) {
-            player1 = 32;
-            cout << "Player 1 got a snake! Sliding down to position 32." << endl;
-        } else if (player1 == 86) {
-            player1 = 56;
-            cout << "Player 1 got a snake! Sliding down to position 56." << endl;
-        } else if (player1 == 99) {
-            player1 = 21;
-            cout << "Player 1 got a snake! Sliding down to position 21." << endl;
-        }
-
-        if (player2 == 14) {
-            player2 = 3;
-            cout << "Player 2 got a ladder! Climbing to position 3." << endl;
-        } else if (player2 == 27) {
-            player2 = 19;
-            cout << "Player 2 got a ladder! Climbing to position 19." << endl;
-        } else if (player2 == 35) {
-            player2 = 9;
-            cout << "Player 2 got a snake! Sliding down to position 9." << endl;
-        } else if (player2 == 50) {
-            player2 = 5;
-            cout << "Player 2 got a snake! Sliding down to position 5." << endl;
-        } else if (player2 == 70) {
-            player2 = 48;
-            cout << "Player 2 got a snake! Sliding down to position 48." << endl;
-        } else if (player2 == 77) {
-            player2 = 32;
-            cout << "Player 2 got a snake! Sliding down to position 32." << endl;
-        } else if (player2 == 86) {
-            player2 = 56;
-            cout << "Player 2 got a snake! Sliding down to position 56." << endl;
-        } else if (player2 == 99) {
-            player2 = 21;
-            cout << "Player 2 got a snake! Sliding down to position 21." << endl;
+            player2=snakeHomeCheck(player2);
         }
     }
 
     // Determine the winner
     if (player1 >= 100) {
         cout << "Player 1 wins!" << endl;
-    } else {
+    } else if (player2 >= 100){
         cout << "Player 2 wins!" << endl;
     }
 
